47b4ae1517096467dc7f4bc28e441a3337947647
jkbwtk <92359584+jkbwtk@users.noreply.github.com>
2024-11-04 22:37:18 +0100

    Create basic file picker component

---
+++ src/components/FilePicker.tsx
@@ -0,0 +1,63 @@
+import { type Setter, mergeProps } from 'solid-js';
+import type { RequiredDefaults } from '#lib/utils';
+import style from '#styles/FilePicker.module.scss';
+
+import Widget from '#components/Widget';
+import MaterialSymbol from '#components/MaterialSymbol';
+import Divider from '#components/Divider';
+
+export type FilePickerProps = {
+  accept?: string;
+  setFile: Setter<File | undefined>;
+  class?: string;
+};
+
+const defaultProps: RequiredDefaults<FilePickerProps> = {
+  accept: '*/*',
+  class: '',
+};
+
+const FilePicker: Component<FilePickerProps> = (unmergedProps) => {
+  const props = mergeProps(defaultProps, unmergedProps);
+
+  function handleFileChange(event: Event) {
+    if (event.target instanceof HTMLInputElement && event.target.files) {
+      props.setFile(event.target.files[0]);
+    }
+  }
+
+  function handleFileDrop(event: DragEvent) {
+    console.log(event);
+    event.preventDefault();
+
+    props.setFile(event.dataTransfer?.files[0]);
+  }
+
+  return (
+    <Widget
+      title="File Picker"
+      component="label"
+      onDrop={handleFileDrop}
+      classList={{
+        [style.container]: true,
+        [props.class]: true,
+      }}
+    >
+      <input
+        class={style.input}
+        onChange={handleFileChange}
+        type="file"
+        accept={props.accept}
+      />
+
+      <div class={style.label}>
+        <span>Drop file here or click </span>
+        <MaterialSymbol symbol="cloud_upload" size="medium" />
+
+        <Divider />
+      </div>
+    </Widget>
+  );
+};
+
+export default FilePicker;
