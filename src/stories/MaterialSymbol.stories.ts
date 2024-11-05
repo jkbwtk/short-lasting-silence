fc6632de68eeb4bd2513c8c6b7cc4162108b11af
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2024-11-06 00:23:24 +0100

    Install and configure storybook

---
+++ src/stories/MaterialSymbol.stories.ts
@@ -0,0 +1,56 @@
+import type { Meta, StoryObj } from 'storybook-solidjs';
+import MaterialSymbol from '#components/MaterialSymbol';
+import { MaterialSymbol as SupportedMaterialSymbols } from '#lib/supportedMaterialSymbols';
+
+const meta = {
+  title: 'Components/MaterialSymbol',
+  component: MaterialSymbol,
+  argTypes: {
+    symbol: { control: 'select', options: SupportedMaterialSymbols },
+    color: {
+      control: 'select',
+      options: ['gray', 'red', 'green', 'blue', 'yellow', 'primary'],
+    },
+    size: {
+      control: 'select',
+      options: ['tiny', 'small', 'alt', 'medium', 'big'],
+    },
+    interactive: { control: 'boolean' },
+    highlightColor: {
+      control: 'select',
+      options: ['none', 'gray', 'red', 'green', 'blue', 'yellow', 'primary'],
+    },
+    filled: { control: 'boolean' },
+    class: { control: 'text' },
+    active: { control: 'boolean' },
+  },
+} as Meta<typeof MaterialSymbol>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    symbol: 'check_box',
+    color: 'primary',
+    size: 'medium',
+    interactive: true,
+    highlightColor: 'yellow',
+    filled: false,
+    class: '',
+    active: false,
+  },
+};
+
+export const Interactive: Story = {
+  args: {
+    symbol: 'cloud_upload',
+    color: 'blue',
+    size: 'big',
+    interactive: true,
+    highlightColor: 'primary',
+    filled: true,
+    class: '',
+    active: true,
+  },
+};
